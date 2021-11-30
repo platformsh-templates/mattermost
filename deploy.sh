c71e3872c69f3cc34848c80eca297f9f43e8e096
chadcarlson <chad.carlson@platform.sh>
2021-11-30 15:03:48 -0500

    Create the subdirectory.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# First deploy details.
+export PSH_INITADMIN_USERNAME=admin
+export PSH_INITADMIN_PASSWORD=Admin1234567!
+export PSH_INITADMIN_EMAIL=admin@example.com
+export PSH_FIRSTTEAM_NAME=team-admin
+export PSH_FIRSTTEAM_DISPLAYNAME=Admin
+export PSH_FIRSTCHANNEL_NAME=setup
+export PSH_FIRSTCHANNEL_DISPLAYNAME=Setup
+export PSH_WELCOME_MESSAGE="Congrats @admin! You have successfully deployed your own Mattermost server on Platform.sh!"
+export PSH_WARNING_MESSAGE1="**WARNING:** This instance has been setup with a default System Admin user credentials to make setup as smooth as possible."
+export PSH_WARNING_MESSAGE2="**WARNING:** Go to your **Account Settings** in the top right of the toolbar and update these credentials immediately."
+
 set_config() {
     if [ ! -f config/config.json ] || [ ! -s config/config.json ]; then
         cp config.default config/config.json
@@ -11,7 +23,8 @@ first_deploy() {
     # Demo details.
     printf "\n\033[1mInitializing Mattermost on first deploy...\033[0m\n"
     printf "\n  ✔ \033[1mCreating initial admin user\033[0m ($PSH_INITADMIN_USERNAME/$PSH_INITADMIN_EMAIL/$PSH_INITADMIN_PASSWORD)\n      "
-    touch $MMCTL_LOCAL_SOCKET_PATH && chmod 600 $MMCTL_LOCAL_SOCKET_PATH
+    mkdir /app/.config/local
+    # touch $MMCTL_LOCAL_SOCKET_PATH && chmod 0600 $MMCTL_LOCAL_SOCKET_PATH
     ./bin/mmctl user create --local --username $PSH_INITADMIN_USERNAME --email $PSH_INITADMIN_EMAIL --password $PSH_INITADMIN_PASSWORD
     # printf "\n  ✔ \033[1mCreating initial private team\033[0m ($PSH_FIRSTTEAM_NAME/$PSH_FIRSTTEAM_DISPLAYNAME)\n    "
     # ./bin/mmctl team create --local --name $PSH_FIRSTTEAM_NAME --display-name $PSH_FIRSTTEAM_DISPLAYNAME --private
