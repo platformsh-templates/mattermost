73447d3d1a9e15316d474e41320e93f83241f0d2
chadcarlson <chad.carlson@platform.sh>
2021-11-30 15:21:25 -0500

    Readd envir.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-# First deploy details.
-export PSH_INITADMIN_USERNAME=admin
-export PSH_INITADMIN_PASSWORD=Admin1234567!
-export PSH_INITADMIN_EMAIL=admin@example.com
-export PSH_FIRSTTEAM_NAME=team-admin
-export PSH_FIRSTTEAM_DISPLAYNAME=Admin
-export PSH_FIRSTCHANNEL_NAME=setup
-export PSH_FIRSTCHANNEL_DISPLAYNAME=Setup
-export PSH_WELCOME_MESSAGE="Congrats @admin! You have successfully deployed your own Mattermost server on Platform.sh!"
-export PSH_WARNING_MESSAGE1="**WARNING:** This instance has been setup with a default System Admin user credentials to make setup as smooth as possible."
-export PSH_WARNING_MESSAGE2="**WARNING:** Go to your **Account Settings** in the top right of the toolbar and update these credentials immediately."
+# # First deploy details.
+# export PSH_INITADMIN_USERNAME=admin
+# export PSH_INITADMIN_PASSWORD=Admin1234567!
+# export PSH_INITADMIN_EMAIL=admin@example.com
+# export PSH_FIRSTTEAM_NAME=team-admin
+# export PSH_FIRSTTEAM_DISPLAYNAME=Admin
+# export PSH_FIRSTCHANNEL_NAME=setup
+# export PSH_FIRSTCHANNEL_DISPLAYNAME=Setup
+# export PSH_WELCOME_MESSAGE="Congrats @admin! You have successfully deployed your own Mattermost server on Platform.sh!"
+# export PSH_WARNING_MESSAGE1="**WARNING:** This instance has been setup with a default System Admin user credentials to make setup as smooth as possible."
+# export PSH_WARNING_MESSAGE2="**WARNING:** Go to your **Account Settings** in the top right of the toolbar and update these credentials immediately."
 
 set_config() {
     if [ ! -f config/config.json ] || [ ! -s config/config.json ]; then
@@ -23,7 +23,7 @@ first_deploy() {
     # Demo details.
     printf "\n\033[1mInitializing Mattermost on first deploy...\033[0m\n"
     printf "\n  ✔ \033[1mCreating initial admin user\033[0m ($PSH_INITADMIN_USERNAME/$PSH_INITADMIN_EMAIL/$PSH_INITADMIN_PASSWORD)\n      "
-    mkdir /app/.config/local
+    source .environment
     # touch $MMCTL_LOCAL_SOCKET_PATH && chmod 0600 $MMCTL_LOCAL_SOCKET_PATH
     ./bin/mmctl user create --local --username $PSH_INITADMIN_USERNAME --email $PSH_INITADMIN_EMAIL --password $PSH_INITADMIN_PASSWORD
     # printf "\n  ✔ \033[1mCreating initial private team\033[0m ($PSH_FIRSTTEAM_NAME/$PSH_FIRSTTEAM_DISPLAYNAME)\n    "
